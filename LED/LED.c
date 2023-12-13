cb436c9949ae8c4f63b0df7548cff2bf5fac6c84
Arthur63 <arthur63@163.com>
2023-12-13 12:46:56 +0800

    fix: fix LED

--- LED/LED.c
+++ LED/LED.c
@@ -1,21 +1,16 @@
 #include <mcs51/8052.h>
 
-#define LED P0_0
-#define ADDR0 P1_0
-#define ADDR1 P1_1
-#define ADDR2 P1_2
-#define ADDR3 P1_3
-#define ENLED P1_4
+#define LED P1_0
 
 void
 main()
 {
-    ENLED = 0;
-    ADDR3 = 1;
-    ADDR2 = 1;
-    ADDR1 = 1;
-    ADDR0 = 0;
-    LED = 0;
-    for (;;)
-        ;
+    for (int i = 0;;) {
+        LED = 0;
+        for (i = 0; i < 30000; i++)
+            ;
+        LED = 1;
+        for (i = 0; i < 30000; i++)
+            ;
+    }
 }
