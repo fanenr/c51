b255bd67b8bf5524a44c0148aa310cb759e20e26
Arthur63 <Arthur63@163.com>
2023-12-18 21:09:20 +0800

    fix: fix led

---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned long u32;
+typedef unsigned int u16;
+typedef unsigned char u8;
+
+typedef long i32;
+typedef int i16;
+typedef char i8;
+
+typedef float f32;
+
+#endif
