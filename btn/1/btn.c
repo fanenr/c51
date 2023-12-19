92b1ab0cec30b7a92d9bdbe8f4f149f10ff82b17
Arthur63 <Arthur63@163.com>
2023-12-19 18:54:51 +0800

    fix: fix btn

--- btn/1/btn.c
+++ btn/1/btn.c
@@ -1,11 +1,26 @@
+#include "types.h"
 #include <mcs51/8052.h>
 
 #define LED0 P1_0
 #define BTN0 P3_0
 
 void
+delay(void)
+{
+    for (u8 i = 0; i < 0xff; i++)
+        ;
+}
+
+void
 main(void)
 {
     for (;;)
-        LED0 = BTN0 == 0 ? 0 : 1;
+        if (BTN0 == 0) {
+            delay();
+            while (BTN0 == 0)
+                ;
+            delay();
+
+            LED0 = !LED0;
+        }
 }
