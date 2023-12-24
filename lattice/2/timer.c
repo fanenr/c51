01b68a74d128f7b759903bc7a7faec772b0f0b5b
Arthur63 <Arthur63@163.com>
2023-12-24 20:24:34 +0800

    fix: fix lattice

--- lattice/2/timer.c
+++ lattice/2/timer.c
@@ -2,26 +2,21 @@
 #include <mcs51/8052.h>
 
 void
-delay(u32 cycles)
+delay_cycles(u32 cycles)
 {
-    ET0 = 0;
+    TMOD &= 0xf0;
+    TMOD |= 0x01;
 
-    u16 times = cycles / 0xffff;
-    u16 residue = 0xffff - cycles % 0xffff;
-
-    TMOD = 1;
-
+    u16 residue = 0xffff - (u16)cycles;
     TL0 = residue;
     TH0 = residue >> 8;
 
     TF0 = 0;
     TR0 = 1;
-    for (;;) {
-        if (TF0 == 1) {
-            break;
-        }
-    }
+    while (TF0 != 1)
+        ;
 
+    u16 times = cycles >> 16;
     for (; times > 0; times--) {
         TL0 = 0;
         TH0 = 0;
@@ -29,6 +24,4 @@ delay(u32 cycles)
         while (TF0 != 1)
             ;
     }
-
-    ET0 = 1;
 }
--- types.h
+++ types.h
@@ -5,14 +5,14 @@ typedef unsigned long u32;
 typedef unsigned int u16;
 typedef unsigned char u8;
 
-typedef long i32;
-typedef int i16;
-typedef char i8;
+typedef signed long i32;
+typedef signed int i16;
+typedef signed char i8;
 
 typedef float f32;
 typedef u8 bool;
 
 #define false 0
-#define true 1
+#define true  1
 
 #endif
