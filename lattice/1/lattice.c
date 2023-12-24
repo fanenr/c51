014b6bcd5b411200257374c62f7f9048db83c0f6
Arthur63 <arthur63@163.com>
2023-12-24 20:24:34 +0800

    fix: fix lattice

--- lattice/1/lattice.c
+++ lattice/1/lattice.c
@@ -6,17 +6,18 @@
 #define RCK P3_6
 
 void
-send_byte(u8 data1, u8 data2)
+send_bytes(u8 hbs, u8 lbs)
 {
     SCK = 0;
+    RCK = 1;
     RCK = 0;
     for (u8 i = 0; i < 8; i++) {
-        SER = data1 & (1 << i);
+        SER = hbs & (1 << i);
         SCK = 1;
         SCK = 0;
     }
     for (u8 i = 0; i < 8; i++) {
-        SER = data2 & (1 << i);
+        SER = lbs & (1 << i);
         SCK = 1;
         SCK = 0;
     }
@@ -27,7 +28,7 @@ send_byte(u8 data1, u8 data2)
 void
 main(void)
 {
-    send_byte(0x7f, 1);
+    send_bytes(0x7f, 1);
     for (;;)
         ;
 }
--- lattice/2/lattice.c
+++ lattice/2/lattice.c
@@ -1,4 +1,5 @@
 #include "timer.h"
+#include "types.h"
 #include <mcs51/8052.h>
 
 #define SER P3_4
@@ -6,12 +7,12 @@
 #define RCK P3_6
 
 void
-send_byte(u8 data1)
+send_byte(u8 lbs)
 {
     SCK = 0;
     RCK = 0;
     for (u8 i = 0; i < 8; i++) {
-        SER = data1 & (1 << i);
+        SER = lbs & (1 << i);
         SCK = 1;
         SCK = 0;
     }
@@ -20,17 +21,18 @@ send_byte(u8 data1)
 }
 
 void
-send_bytes(u8 data1, u8 data2)
+send_bytes(u8 hbs, u8 lbs)
 {
     SCK = 0;
+    RCK = 1;
     RCK = 0;
     for (u8 i = 0; i < 8; i++) {
-        SER = data1 & (1 << i);
+        SER = hbs & (1 << i);
         SCK = 1;
         SCK = 0;
     }
     for (u8 i = 0; i < 8; i++) {
-        SER = data2 & (1 << i);
+        SER = lbs & (1 << i);
         SCK = 1;
         SCK = 0;
     }
@@ -47,7 +49,7 @@ show_lattice(void)
         if (lattice[i] == 0)
             continue;
         send_bytes(~(0x80 >> i), lattice[i]);
-        delay(100);
+        delay_msecs(1);
     }
 }
 
