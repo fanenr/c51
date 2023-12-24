54f1f828c8ee4bce69fb61a49578416ceddf2008
Arthur63 <Arthur63@163.com>
2023-12-24 12:49:51 +0800

    fix: fix led btn btns

--- btn/2/btn.c
+++ btn/2/btn.c
@@ -1,3 +1,4 @@
+#include "timer.h"
 #include "types.h"
 #include <mcs51/8052.h>
 
@@ -5,22 +6,15 @@
 #define BTN0 P3_0
 
 void
-delay(void)
-{
-    for (u8 i = 0; i < 0xff; i++)
-        ;
-}
-
-void
 main(void)
 {
     u8 times = 0;
     for (;;)
         if (BTN0 == 0) {
-            delay();
+            delay_msecs(10);
             while (BTN0 == 0)
                 ;
-            delay();
+            delay_msecs(10);
 
             LED = ~(1 << (times % 8));
             times++;
