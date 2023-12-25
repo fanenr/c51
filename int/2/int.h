f4c4085a141de7107bbea73e9d63562e727f24b4
Arthur63 <Arthur63@163.com>
2023-12-25 13:30:03 +0800

    feat: add int2

---
+++ int/2/int.h
@@ -0,0 +1,20 @@
+#ifndef INT_H
+#define INT_H
+
+#include "types.h"
+
+extern void loop_int_cycles(u16 cycles);
+
+#define US_PER_MCYCLE   1.085
+#define MAX_LOOP_INT_MS 71.0
+
+static inline void
+loop_int_msecs(float msecs)
+{
+    if (msecs >= MAX_LOOP_INT_MS)
+        loop_int_cycles(0xffff);
+    else
+        loop_int_cycles(1000 * msecs / US_PER_MCYCLE);
+}
+
+#endif
