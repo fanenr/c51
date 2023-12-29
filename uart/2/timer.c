096263a60f40f2fb28a0eab6bf5dc0aa9ab61539
Arthur63 <Arthur63@163.com>
2023-12-29 14:01:06 +0800

    feat: add uart2

---
+++ uart/2/timer.c
@@ -0,0 +1,27 @@
+#include "timer.h"
+#include <mcs51/8052.h>
+
+void
+delay_cycles(u32 cycles)
+{
+    TMOD &= 0xf0;
+    TMOD |= 0x01;
+
+    u16 residue = 0xffff - (u16)cycles;
+    TL0 = residue;
+    TH0 = residue >> 8;
+
+    TF0 = 0;
+    TR0 = 1;
+    while (TF0 != 1)
+        ;
+
+    u16 times = cycles >> 16;
+    for (; times > 0; times--) {
+        TL0 = 0;
+        TH0 = 0;
+        TF0 = 0;
+        while (TF0 != 1)
+            ;
+    }
+}
---
+++ uart/2/uart.c
@@ -0,0 +1,39 @@
+#include "btns.h"
+#include "timer.h"
+#include "types.h"
+#include <mcs51/8052.h>
+
+void
+config_uart(u16 baud)
+{
+    SCON = 0x50;
+    TMOD &= 0x0f;
+    TMOD |= 0x20;
+    TL1 = TH1 = 256 - 28800 / baud;
+    ES = EA = 1;
+    ET1 = 0;
+    TR1 = 1;
+}
+
+void
+int_uart(void) __interrupt(4)
+{
+}
+
+void
+main(void)
+{
+    config_uart(4800);
+
+    for (u8 val;;) {
+        val = scan_btns();
+
+        if (val == 0)
+            continue;
+
+        SBUF = val;
+        while (TI == 0)
+            ;
+        TI = 0;
+    }
+}
