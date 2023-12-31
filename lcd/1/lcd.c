6ff962c95cb1eda82c92f7f8992cc4ca08644626
Arthur63 <Arthur63@163.com>
2023-12-31 10:15:49 +0800

    tweak: tweak lcd module

--- lcd/1/lcd.c
+++ lcd/1/lcd.c
@@ -3,11 +3,80 @@
 
 #define LCD_DB P0
 #define LCD_RS P3_5
-#define LCD_WR P3_6
+#define LCD_RW P3_6
 #define LCD_EN P3_4
+
+void init_lcd(void);
+void show_lcd(u8 x, u8 y, const u8 *str);
 
 void
 main(void)
 {
+    init_lcd();
 
+    const u8 *str = "Hello World!";
+    show_lcd(2, 0, str);
+    show_lcd(1, 1, "Love C Forever");
+
+    for (;;)
+        ;
+}
+
+static void
+wait(void)
+{
+    LCD_DB = 0xFF;
+    LCD_RS = 0;
+    LCD_RW = 1;
+
+    u8 sts;
+    do {
+        LCD_EN = 1;
+        sts = LCD_DB;
+        LCD_EN = 0;
+    } while (sts & 0x80);
+}
+
+static void
+write_cmd(u8 cmd)
+{
+    wait();
+    LCD_RS = 0;
+    LCD_RW = 0;
+    LCD_DB = cmd;
+    LCD_EN = 1;
+    LCD_EN = 0;
+}
+
+static void
+write_data(u8 ch)
+{
+    wait();
+    LCD_RS = 1;
+    LCD_RW = 0;
+    LCD_DB = ch;
+    LCD_EN = 1;
+    LCD_EN = 0;
+}
+
+void
+show_lcd(u8 x, u8 y, const u8 *str)
+{
+    /* set cursor */
+    if (y != 0)
+        x += 0x40;
+    write_cmd(x | 0x80);
+
+    /* send data */
+    while (*str != '\0')
+        write_data(*str++);
+}
+
+void
+init_lcd(void)
+{
+    write_cmd(0x38);
+    write_cmd(0x0C);
+    write_cmd(0x06);
+    write_cmd(0x01);
 }
