1009567db51449e5cea7550e0dbe1371a60736b7
Arthur63 <Arthur63@163.com>
2024-01-02 12:36:03 +0800

    tweak: tweak uart1

--- uart/1/uart.c
+++ uart/1/uart.c
@@ -6,9 +6,10 @@ void
 config_uart(u16 baud)
 {
     SCON = 0x50;
-    TMOD &= 0x0F;
+    TMOD &= 0x0f;
     TMOD |= 0x20;
-    TL1 = TH1 = 256 - (11059200 / 12 / 32) / baud;
+    TL1 = TH1 = 256 - 28800 / baud;
+    ES = EA = 1;
     ET1 = 0;
     TR1 = 1;
 }
