d75f157edd699d402bc379179d4713ccd67aca3b
Arthur63 <arthur63@163.com>
2024-04-16 09:26:46 +0800

    update tube

--- btns.c
+++ btns.c
@@ -3,19 +3,19 @@
 #include <mcs51/8052.h>
 
 u8
-scan_btns (void)
+btns_scan (void)
 {
-  u8 val = 1;
+  u8 ret = 1;
 
   P3 = 0xf0;
   if (P3 == 0xf0)
     return 0;
   else if (P3_5 == 0)
-    val = 2;
+    ret = 2;
   else if (P3_6 == 0)
-    val = 3;
+    ret = 3;
   else if (P3_7 == 0)
-    val = 4;
+    ret = 4;
 
   delay_msecs (10);
 
@@ -23,16 +23,16 @@ scan_btns (void)
   if (P3 == 0x0f)
     return 0;
   else if (P3_1 == 0)
-    val += 4;
+    ret += 4;
   else if (P3_2 == 0)
-    val += 8;
+    ret += 8;
   else if (P3_3 == 0)
-    val += 12;
+    ret += 12;
 
   while (P3 != 0x0f)
     ;
 
   delay_msecs (10);
 
-  return val;
+  return ret;
 }
---
+++ tube.h
@@ -0,0 +1,18 @@
+#ifndef TUBE_H
+#define TUBE_H
+
+#include "type.h"
+
+/* tube_data holds 8 chars */
+extern u8 tube_data[];
+
+/*
+ * 0 - 9: 0 1 2 3 4 5 6 7 8 9
+ * 10-18: A B C d E F H L -
+ */
+extern __code u8 tube_char[];
+
+/* show tube_data */
+extern void tube_show (void);
+
+#endif
